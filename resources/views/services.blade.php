0e55956ce44e281629d579ae7b7bd94d4963093e
jeremihz <mihjere68@gmail.com>
2019-09-24 17:40:01 +0300

    services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,126 +1,149 @@
 @extends('layouts.app')
 
 @section('content')
-	<header class="service">
-	  <div class="container h-100">
-	    <div class="row h-100 align-items-center">
-	      <div class="col-12 text-center">
-	        <h1 class="font-weight-light">List of Services we Offer</h1>
-	        <p class="lead">These are the simple steps one can follow to me an order</p>
-	      </div>
-	    </div>
-	  </div>
-	</header>
-	<div class="justify-content-center py-3">
+
+
+<link href="{{ asset('css/services.css') }}" rel="stylesheet">
+<br>
+<br>
+<br>
+  
+  <div class="justify-content-center py-3">
         <div class="row justify-content-center">
            <h2 class="featurette px-4">Academic Paper Writing Service</h2>
        </div>
-       <div class="row justify-content-center container-fluid pt-3">
-           <div class="col-md-4">
-               <ul class="list-group">
-                 <li class="list-group-item">Accounting</li>
-                 <li class="list-group-item">Advertising</li>
-                 <li class="list-group-item">Aeronautical Engineering</li>
-                 <li class="list-group-item">Aeronautics</li>
-                 <li class="list-group-item">Aerospace Science</li>
-                 <li class="list-group-item">African-American Studies</li>
-                 <li class="list-group-item">Agricultural Studies</li>
-                 <li class="list-group-item">American History</li>
-                 <li class="list-group-item">American-Literature</li>
-                 <li class="list-group-item">Anthropology</li>
-                 <li class="list-group-item">Antique Literature</li>
-                 <li class="list-group-item">Application Essay</li>
-                 <li class="list-group-item">Architecture</li>
-                 <li class="list-group-item">Art</li>
-                 <li class="list-group-item">Asian Literature</li>
-                 <li class="list-group-item">Asian Studies</li>
-                 <li class="list-group-item">Astronomy</li>
-                 <li class="list-group-item">Aviation</li>
-                 <li class="list-group-item">Biology</li>
-                 <li class="list-group-item">Business</li>
-                 <li class="list-group-item">Canadian Studies</li>
-                 <li class="list-group-item">Case Study</li>
-                 <li class="list-group-item">Chemistry</li>
-                 <li class="list-group-item">Civil &amp; Structural Engineering</li>
-                 <li class="list-group-item">Communications and Media</li>
-                 <li class="list-group-item">Communications Strategies</li>
-                 <li class="list-group-item">Company analysis</li>
-                 <li class="list-group-item">Computer Science</li>
-                 <li class="list-group-item">Creative Writing</li>
-                 <li class="list-group-item">Criminology</li>
-                 <li class="list-group-item">Dance</li>
-               </ul>
-           </div>
-           <div class="col-md-4">
-               <ul class="list-group">
-                 <li class="list-group-item">Design analysis</li>
-                 <li class="list-group-item">Drama</li>
-                 <li class="list-group-item">East European Studies</li>
-                 <li class="list-group-item">E-Commerce</li>
-                 <li class="list-group-item">Economics</li>
-                 <li class="list-group-item">Education</li>
-                 <li class="list-group-item">Education Theories</li>
-                 <li class="list-group-item">Engineering</li>
-                 <li class="list-group-item">English</li>
-                 <li class="list-group-item">Environmental Issues</li>
-                 <li class="list-group-item">Ethics</li>
-                 <li class="list-group-item">Film</li>
-                 <li class="list-group-item">Finance</li>
-                 <li class="list-group-item">Geography</li>
-                 <li class="list-group-item">Healthcare</li>
-                 <li class="list-group-item">History</li>
-                 <li class="list-group-item">Holocaust</li>
-                 <li class="list-group-item">Information Technology</li>
-                 <li class="list-group-item">Internet</li>
-                 <li class="list-group-item">Investment</li>
-                 <li class="list-group-item">IT Management</li>
-                 <li class="list-group-item">Journalism</li>
-                 <li class="list-group-item">Latin-American Studies</li>
-                 <li class="list-group-item">Law</li>
-                 <li class="list-group-item">Legal Issues</li>
-                 <li class="list-group-item">Linguistics</li>
-                 <li class="list-group-item">Literature</li>
-                 <li class="list-group-item">Logistics</li>
-                 <li class="list-group-item">Management</li>
-                 <li class="list-group-item">Marketing</li>
-                 <li class="list-group-item">Mathematics</li>
-               </ul>
-           </div>
-           <div class="col-md-4">
-               <ul class="list-group">
-                 <li class="list-group-item">Mechanical Engineering</li>
-                 <li class="list-group-item">Medicine and Health Studies</li>
-                 <li class="list-group-item">Morbi leo risus</li>
-                 <li class="list-group-item">Movies</li>
-                 <li class="list-group-item">Music</li>
-                 <li class="list-group-item">Native-American Studies</li>
-                 <li class="list-group-item">Nature</li>
-                 <li class="list-group-item">Nursing</li>
-                 <li class="list-group-item">Nutrition</li>
-                 <li class="list-group-item">Paintings</li>
-                 <li class="list-group-item">Pedagogy</li>
-                 <li class="list-group-item">Pharmacology</li>
-                 <li class="list-group-item">Philosophy</li>
-                 <li class="list-group-item">Physics</li>
-                 <li class="list-group-item">Political Science</li>
-                 <li class="list-group-item">Psychology</li>
-                 <li class="list-group-item">Public Relations</li>
-                 <li class="list-group-item">Religion and Theology</li>
-                 <li class="list-group-item">Religious Studies</li>
-                 <li class="list-group-item">Shakespeare Literature</li>
-                 <li class="list-group-item">Sociology</li>
-                 <li class="list-group-item">Sports</li>
-                 <li class="list-group-item">Statistics</li>
-                 <li class="list-group-item">Teacher’s Career</li>
-                 <li class="list-group-item">Technology</li>
-                 <li class="list-group-item">Theatre</li>
-                 <li class="list-group-item">Tourism</li>
-                 <li class="list-group-item">Trade</li>
-                 <li class="list-group-item">Web Design</li>
-                 <li class="list-group-item">West-European Studies</li>
-               </ul>
-           </div>
-       </div>
+	<section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Services</h5>
+            
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Accounting</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Advertising</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Aeronautical Engineering</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Aeronautics</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Aerospace Science</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>African-American Studies</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Agricultural Studies</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>American History</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>American-Literature</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Anthropology</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Antique Literature</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Application Essay</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Architecture</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Art</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Asian Literature</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Asian Studies</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Astronomy</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Aviation</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Biology</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Business</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Canadian Studies</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Case Study</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Chemistry</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Civil &amp; Structural Engineering</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Communications and Media</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Communications Strategies</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Company analysis</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Computer Science</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Creative Writing</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Criminology</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dance</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Services</h5>
+           
+            <hr>
+            <ul class="fa-ul">
+                          <li><span class="fa-li"><i class="fas fa-check"></i></span>Design analysis</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Drama</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>East European Studies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>E-Commerce</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Economics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Education</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Education Theories</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Engineering</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>English</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Environmental Issues</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Ethics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Film</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Finance</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Geography</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Healthcare</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>History</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Holocaust</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Information Technology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Internet</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Investment</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>IT Management</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Journalism</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Latin-American Studies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Law</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Legal Issues</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Linguistics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Literature</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Logistics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Management</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Marketing</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Mathematics</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Services</h5>
+            
+            <hr>
+            <ul class="fa-ul">
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Mechanical Engineering</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Medicine and Health Studies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Morbi leo risus</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Movies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Music</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Native-American Studies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Nature</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Nursing</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Nutrition</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Paintings</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Pedagogy</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Pharmacology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Philosophy</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Physics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Political Science</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Psychology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Public Relations</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Religion and Theology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Religious Studies</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Shakespeare Literature</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Sociology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Sports</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Statistics</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Teacher’s Career</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Technology</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Theatre</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Tourism</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Trade</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Web Design</li>
+                 <li><span class="fa-li"><i class="fas fa-check"></i></span>West-European Studies</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    @include('includes.footer')
+  </div>
+</section>    @include('includes.footer')
 @endsection
